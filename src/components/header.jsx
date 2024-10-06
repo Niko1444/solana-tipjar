ac1d84606acd161827d60d3e68609d39289665b5
Le Tien Phat <91601109+Niko1444@users.noreply.github.com>
2024-10-06 12:33:57 +0700

    fix signout button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -130,7 +130,6 @@ export default function Header() {
                     >
                       Sign Out
                     </button>
-                    b
                   </div>
                 </div>
               </div>
